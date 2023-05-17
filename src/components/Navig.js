b4f0451e07528520a5985459d089e09958a8c0e1
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-05-18 02:00:31 +0300

    commit

--- src/components/Navig.js
+++ src/components/Navig.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Drawer from '@mui/material/Drawer';
+import { makeStyles } from 'tss-react/mui'
 
+const useStyles=makeStyles({
+    navLogo: {
+       display:'flex'
+    }
+});
 
 const Navig=()=>{
+    const classes=useStyles();
     return (
         <div>
-            <Drawer >
-              
+            <Drawer variant="permanent" open={true}>
+                <div className={classes.navLogo}>
+                    <h2>Invoice Reader App</h2>
+                </div>
+             
             </Drawer>
         </div>
     )
