97244b92ab06278253e8c06ea6136879c6b994d3
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-05-18 00:51:01 +0300

    commit

---
+++ src/components/Navig.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Drawer from '@mui/material/Drawer';
+
+
+const Navig=()=>{
+    return (
+        <div>
+            <Drawer >
+              
+            </Drawer>
+        </div>
+    )
+}
+
+export default Navig
