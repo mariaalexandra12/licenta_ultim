cc95a6d750c3c6a778abeb1d090a405381c06317
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-05-31 22:56:09 +0300

    commit

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 
-
-const MenuItem=()=>{
+const MenuItem=(label)=>{
     return 
-    <></>
+    <Button>{label}</Button>
 }
 
 export default MenuItem
--- src/components/Navig.js
+++ src/components/Navig.js
@@ -2,34 +2,17 @@ import React from "react";
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import MenuItem from './MenuItem';
-import { makeStyles, withStyles } from 'tss-react/mui'
-import invoice from "./images/invoice.png"
-import  Button  from "@mui/material/Button";
+import Button from '@mui/material/Button'
 
-const styles = makeStyles({
-    navlogo:{
-        width:20,
-    },
-
-    navigationLogoContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    },
-
-    navigationDrawer: {
-        width:240,
-    }
-}); 
 
 const Navig=()=>{
-    const classes =styles();
     return (
-        <div className={classes.navigationLogoContainer}>
+        <div>
             <Drawer variant="permanent" open={true}>
                 <div>
                   <h1>Invoice Reader App</h1>
-                  <Button>First MaterialUi</Button>
+                  <Button variant="contained" 
+                  onClick={()=>console.log("Invoice Reader App")}>First Mui com</Button>
                 </div>
             </Drawer>
         </div>
