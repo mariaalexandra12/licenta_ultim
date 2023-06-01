5a27b8bd901d7672dd6839be8db9dfac818b5a35
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-01 13:58:41 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,13 +1,21 @@
+import React from 'react';
 
 
 
-function AdaugaFacturi(){
-    return (
-       <div>
-          <h1 style={{marginLeft:'400px'}}>Incarca Factura</h1>   
-       </div>
-    )
+function AdaugaFacturi() {
 
+
+  return (
+    <div>
+       <h1 style={{marginLeft:"500px"}}>Incarca Facturi</h1>
+       <form style={{marginLeft:"500px"}}>
+         <div className="formElements">
+            <input type='file' onChange={(e)=>{console.log(e.target.files[0])}}></input>
+            <button>Show file</button>
+         </div>
+       </form>
+    </div>
+  );
 }
 
 export default AdaugaFacturi
