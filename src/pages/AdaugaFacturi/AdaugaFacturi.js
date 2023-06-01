c113e94c6d2dcaf6f1d8bd03db6df9a45a111d58
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-01 15:03:59 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 function AdaugaFacturi() {
-
-
+  const [selectedImage,setSelectedImage]=useState();
+  const imageChange=(event)=>{
+    if(event.target.files && event.target.files.length >0){
+        setSelectedImage(event.target.files[0]);
+    }
+  }  
+  
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    alert(URL.createObjectURL(selectedImage));
+  }
   return (
     <div>
        <h1 style={{marginLeft:"500px"}}>Incarca Facturi</h1>
-       <form style={{marginLeft:"500px"}}>
+       <form onSubmit={onSubmit} style={{marginLeft:"250px"}}>
          <div className="formElements">
-            <input type='file' onChange={(e)=>{console.log(e.target.files[0])}}></input>
-            <button>Show file</button>
+            <div className="row">
+            <label>Alege factura</label>
+            <br/>
+            <input type='file' onChange={imageChange}></input>
+            </div><br/>
+            <button type='submit'>Upload file</button>
          </div>
+         <div style={{
+                marginTop:"20px",
+                marginLeft:"0px",
+                display: "flex",
+                flexDirection: "column",
+            }}>
+                <img src={URL.createObjectURL(selectedImage)} alt="img"
+                style={{
+                    maxHeight:"420px",
+                    maxWidth:"400px",    
+                }}/>
+            </div>
        </form>
     </div>
   );
