1615d3ba0ff7c5a4b295ba39fdf74bfa57ff90f6
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-06 13:34:38 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -8,6 +8,9 @@ import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 // default layout plugin
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import packageJson from '/package.json';
+
+
 
 // Import styles
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
@@ -16,6 +19,7 @@ function AdaugaFacturi() {
 
   // creating new plugin instance
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const pdfjsVersion = packageJson.dependencies['pdfjs-dist'];
 
   // pdf file onChange state
   const [pdfFile, setPdfFile]=useState(null);
@@ -72,7 +76,7 @@ function AdaugaFacturi() {
 
         {/* render this if we have a pdf file */}
         {pdfFile&&(
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.min.js">
+          <Worker workerUrl='https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js'>
             <Viewer fileUrl={pdfFile}
             plugins={[defaultLayoutPluginInstance]}></Viewer>
           </Worker>
