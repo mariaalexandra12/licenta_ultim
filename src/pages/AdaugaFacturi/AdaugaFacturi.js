5cb6872a86a40bf8860a3c25e38b476137f31e62
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-06 23:16:33 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { Worker , Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 export default function AdauagaFacturi(){
+
+  const [pdfFile,setPdfFile]=useState(null)
+  const [viewPdf,setViewPdf]=useState(null)
+  const [error , setError]=useState(null)
+  const fileType=['application/pdf']
+  const handleChange=(event)=>{
+     let selectedFile=event.target.files[0]
+     if(selectedFile){
+        if(selectedFile && fileType.include(selectedFile.type)){
+            let reader=new FileReader()
+            reader.readAsDataURL(selectedFile)
+            reader.onload=(event)=>{
+              setPdfFile(event.target.result)
+            }
+        }
+        else{
+          setPdfFile(null)
+          alert('Ai introdus un format invalid! Incarca doar fisiere PDF!')
+        }
+     }
+  }
+
+  const handleSubmit=(e)=>{
+        e.preventDefault()
+        if(pdfFile !== null){
+          setViewPdf(pdfFile)
+        }
+        else{
+          setPdfFile(null)
+        }
+  }
+
+
   return(
     <div class="pdfwork">
-      <span>Select PDF</span>
-      <input type="file" class="selectpdf"></input>
-      <button class="upload">Upload</button>
-      <div class="afterupload">
-         
-         
-      </div>
+      <form>
+          <input type="file" className="form-control" onChange={handleChange}></input>        
+          <button type="submit" onClick={handleSubmit}>View Pdf</button>
+      </form>
+      <h2>View Pdf</h2>
+        <div className="pdfView" style={{
+        width:"500px",
+        height:"500px",
+        display:"flex",
+        overflowY:"auto",
+        justifyContent:"center",
+        alignItems:"center",
+       }}>
+        
+        
+       
+      
+        </div>
     </div>
+    
   )
 }
