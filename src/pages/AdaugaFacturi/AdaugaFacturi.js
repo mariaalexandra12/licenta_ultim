8017fc506ef2ff6dada564f24b278525328738f4
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-06 04:01:52 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
-const AdaugaFacturi=()=>{
+function AdaugaFacturi(){
+
+   const [pdfFile, setPdfFile]=useState(null);
+   const [pdfError, setPdfError]=useState('');
+
+   const fisierePermise=['application/pdf']
+   const handleFile=(e)=>{
+      let selectedFile=e.target.files[0];
+      if(selectedFile){
+        if(selectedFile&&fisierePermise.includes(selectedFile.type)){
+            let reader=new FileReader();
+            reader.readAsDataURL(selectedFile);
+            reader.onloadend=(e)=>{
+               setPdfError('');
+               setPdfFile(e.target.result);
+               console.log(e.target.result);
+            }
+        }
+        else{
+         setPdfError('Ai incarcat un document care nu are un format valid. Incarca un fisier de tip pdf!');
+        }
+      }
+      else{
+         console.log('Please select a PDF file');
+      }
+   }
+
    return(
-    <>
-    <h1>Buna</h1>
-    
-    </>
+      
+      <div className="container">
+         <form>
+          <label><h5>Adauga o factura</h5></label>    
+          <br/>
+          <input type='file' className="form-control"
+          onChange={handleFile}></input>     
+          {pdfError&& <span className="text">{pdfError}</span>}
+      </form>
+
+      <h5>Vizualizare Factura</h5>
+      <div className="viewer">
+          {pdfFile&&(
+            <div>Afisare Factura</div>
+          )}
+          {!pdfFile && <>Nu a fost selectat niciun fisier</>}
+      </div>
+      </div>
+   
    )
 }
 
