25fe8cad6b44e2574a0023e060d6582cf02eac3f
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-06 04:23:15 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,54 +1,90 @@
-import React from 'react';
-import { useState } from 'react';
+import {useState} from 'react'
 
-function AdaugaFacturi(){
+// Import Worker
+import { Worker } from '@react-pdf-viewer/core';
+// Import the main Viewer component
+import { Viewer } from '@react-pdf-viewer/core';
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+// default layout plugin
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
-   const [pdfFile, setPdfFile]=useState(null);
-   const [pdfError, setPdfError]=useState('');
+// Import styles
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-   const fisierePermise=['application/pdf']
-   const handleFile=(e)=>{
-      let selectedFile=e.target.files[0];
-      if(selectedFile){
-        if(selectedFile&&fisierePermise.includes(selectedFile.type)){
-            let reader=new FileReader();
-            reader.readAsDataURL(selectedFile);
-            reader.onloadend=(e)=>{
-               setPdfError('');
-               setPdfFile(e.target.result);
-               console.log(e.target.result);
-            }
-        }
-        else{
-         setPdfError('Ai incarcat un document care nu are un format valid. Incarca un fisier de tip pdf!');
+function AdaugaFacturi() {
+
+  // creating new plugin instance
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  // pdf file onChange state
+  const [pdfFile, setPdfFile]=useState(null);
+
+  // pdf file error state
+  const [pdfError, setPdfError]=useState('');
+
+
+  // handle file onChange event
+  const allowedFiles = ['application/pdf'];
+  const handleFile = (e) =>{
+    let selectedFile = e.target.files[0];
+    // console.log(selectedFile.type);
+    if(selectedFile){
+      if(selectedFile&&allowedFiles.includes(selectedFile.type)){
+        let reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend=(e)=>{
+          setPdfError('');
+          setPdfFile(e.target.result);
         }
       }
       else{
-         console.log('Please select a PDF file');
+        setPdfError('Not a valid pdf: Please select only PDF');
+        setPdfFile('');
       }
-   }
+    }
+    else{
+      console.log('please select a PDF');
+    }
+  }
 
-   return(
-      
-      <div className="container">
-         <form>
-          <label><h5>Adauga o factura</h5></label>    
-          <br/>
-          <input type='file' className="form-control"
-          onChange={handleFile}></input>     
-          {pdfError&& <span className="text">{pdfError}</span>}
+  return (
+    <div className="container">
+
+      {/* Upload PDF */}
+      <form>
+
+        <label><h5>Upload PDF</h5></label>
+        <br></br>
+
+        <input type='file' className="form-control"
+        onChange={handleFile}></input>
+
+        {/* we will display error message in case user select some file
+        other than pdf */}
+        {pdfError&&<span className='text-danger'>{pdfError}</span>}
+
       </form>
 
-      <h5>Vizualizare Factura</h5>
+      {/* View PDF */}
+      <h5>View PDF</h5>
       <div className="viewer">
-          {pdfFile&&(
-            <div>Afisare Factura</div>
-          )}
-          {!pdfFile && <>Nu a fost selectat niciun fisier</>}
+
+        {/* render this if we have a pdf file */}
+        {pdfFile&&(
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.min.js">
+            <Viewer fileUrl={pdfFile}
+            plugins={[defaultLayoutPluginInstance]}></Viewer>
+          </Worker>
+        )}
+
+        {/* render this if we have pdfFile state null   */}
+        {!pdfFile&&<>No file is selected yet</>}
+
       </div>
-      </div>
-   
-   )
+
+    </div>
+  );
 }
 
-export default AdaugaFacturi
+export default AdaugaFacturi;
