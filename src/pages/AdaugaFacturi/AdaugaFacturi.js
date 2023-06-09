ce8b30d9c20b7f50493e6119d26599624d3efcdd
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-09 21:24:41 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -6,20 +6,23 @@ import { Image } from "@mui/icons-material";
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-
+import "./AdaugaFacturi.css";
 
 export default function AdauagaFacturi(){
 
-
-
   return(
     <div className="pdfwork">
-      
-     BUNA 
-     
+    <link rel="stylesheet" type="AdaugaFacturi.css"></link>
+    
+
+      <div className="dragDrop">
 
 
-    </div>
+      </div>
+
+
+
+      </div>
     
   )
 }
