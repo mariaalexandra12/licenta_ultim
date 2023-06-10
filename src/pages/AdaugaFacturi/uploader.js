ec5c0bb936a2d25e9ed6126865649f45abcd7fc3
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-10 13:39:53 +0300

    commit

--- src/pages/AdaugaFacturi/uploader.js
+++ src/pages/AdaugaFacturi/uploader.js
@@ -3,10 +3,42 @@ import "./uploader.css"
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { createWorker } from "tesseract.js";
 
-export function Uploader(){
+export function Uploader({onReadOcrData}){
+    const STATUSES={
+        IDLE:"",
+        FAILED:"Failed to perform OCR",
+        PENDIND:"Processing...This may take a while",
+        SUCCEEDED:"Completed",
+    }
 
     const [fileName,setFileNames]=useState();
     const[image,setImage]=useState(null);
+    const [ocrState, setOcrState]=useState(STATUSES.IDLE);
+    const worker=createWorker();
+
+    //PROCESS
+    const readImage=async()=>{
+        setOcrState(STATUSES.PENDIND);
+        try{
+            //am setat limba
+            await worker.load()
+            await worker.loadLanguage("ron")
+            await worker.initialize("ron")
+            //recunoaste textul de pe factura 
+            const {data : {text}} = (await worker).recognize(image)
+            (await worker).terminate()
+
+            onReadOcrData(text)
+            setOcrState(STATUSES.SUCCEEDED)
+        }
+        catch(err){
+            setOcrState(STATUSES.FAILED)
+        }
+    }
+   
+
+
+
 
    return(    
     <div className="adaugaFact">
