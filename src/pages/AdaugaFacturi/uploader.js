77f70c7142fa0ad2d14d09ab5463ab11117ada1f
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-10 12:55:26 +0300

    commit

--- src/pages/AdaugaFacturi/uploader.js
+++ src/pages/AdaugaFacturi/uploader.js
@@ -1,56 +1,37 @@
 import React, { useState } from 'react';
 import "./uploader.css"
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import Tesseract from "tesseract.js";
+import { createWorker } from "tesseract.js";
 
 export function Uploader(){
-    
-    const [extractedData, setExtractedData] = useState(null);
 
-    const extractDataFromPDF = async (file) => {
-      const worker = Tesseract.createWorker();
-      //await worker.load();
-      await worker.loadLanguage('ron');
-      await worker.initialize('ron');
-      const { data: { text } } = await worker.recognize(file);
-      await worker.terminate();
-  
-      const extractedValues = extractValuesFromText(text);
-      setExtractedData(extractedValues);
-    };
-  
-    const extractValuesFromText = (text) => {
-  
-      const regex = /Total de plata:\s*(\d+\.\d+)/;
-      const match = text.match(regex);
-      const totalValue = match ? match[1] : '';
-  
-      return {
-        total: totalValue,
-  
-      };
-    };
-
-
-
+    const [fileName,setFileNames]=useState();
+    const[image,setImage]=useState(null);
 
    return(    
     <div className="adaugaFact">
      <form action="">
-        <input type="file" className="inputFile" hidden={true} onChange={(event) => extractDataFromPDF(event.target.files[0])}
-        ></input>
+        <input type="file" className="inputFile" hidden={true} onChange={({target:{files}})=>{
+            files[0] && setFileNames(files[0].name);
+            if(files){
+                setImage(URL.createObjectURL(files[0]));
+            }
+        }}></input>
         <CloudUploadIcon className="uploadIcon" onClick={()=>document.querySelector(".inputFile").click()}>
 
         </CloudUploadIcon>
      
         <p>Incarca o factura</p>
+        <section>
+          {fileName ?
+           <>A fost selectat documentul "{fileName}"</>
+           :
+           <>Nu a fost selectat un document</>
+           }
+        </section>
         
      </form>
-     {extractedData && (
-        <div>
-          <p>Total factură: {extractValuesFromText.totalValue}</p>
-        </div>
-      )}
+    
      
      
      
