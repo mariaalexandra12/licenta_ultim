835d8fa4b67fbc7640e73406ecba245090557945
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-10 13:44:33 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -8,7 +8,9 @@ export default function AdauagaFacturi(){
  
   return(
     
+    <div className="container">
     <Uploader/>
+    </div>
     
   )
 }
--- src/pages/AdaugaFacturi/OcrData.js
+++ src/pages/AdaugaFacturi/OcrData.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { createWorker } from 'tesseract.js';
 
-export default function OcrData(){
+export default function OcrData({onReadOcrData}){
+
+
+    const STATUSES={
+        IDLE:"",
+        FAILED:"Failed to perform OCR",
+        PENDIND:"Processing...This may take a while",
+        SUCCEEDED:"Completed",
+    }
+
+    
+    const [ocrState, setOcrState]=useState(STATUSES.IDLE);
+    const worker=createWorker();
+
+    //PROCESS
+    const readImage=async()=>{
+        setOcrState(STATUSES.PENDIND);
+        try{
+            //am setat limba
+            await worker.load()
+            await worker.loadLanguage("ron")
+            await worker.initialize("ron")
+            //recunoaste textul de pe factura 
+            const {data : {text}} = (await worker).recognize()
+            (await worker).terminate()
+
+            onReadOcrData(text)
+            setOcrState(STATUSES.SUCCEEDED)
+        }
+        catch(err){
+            setOcrState(STATUSES.FAILED)
+        }
+    }
    return (
 
    
