8ac17ea41812f6e84e477ce69df81a2a9ae9c357
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-10 18:42:30 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
-import { Uploader } from "./uploader";
-import OcrData from "./OcrData";
+import { createWorker } from "tesseract.js";
+import "./uploader.css";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 
 export default function AdauagaFacturi(){
 
- 
-  return(
-    
-    <div className="container">
-    <Uploader/>
+  const [selectedImage, setSelectedImage ]=useState(null)
+  const [ocrResult , setOcrResult] = useState("")
 
-    </div>
+  const handleChangeImage=e=>{
+    setSelectedImage(e.target.files[0]);
+  }
+
+ 
+ 
+  return(    
+    <div className="adaugaFact">
+        <div className="imageUploadDisplay">
+     <form action="">
+        <input type="file" className="inputFile" hidden={true} onChange={handleChangeImage}></input>
+        <CloudUploadIcon className="uploadIcon" onClick={()=>document.querySelector(".inputFile").click()}>
+
+        </CloudUploadIcon>
+     
+        <p>Incarca o factura</p>
+        
+     </form>
+   
     
-  )
+
+   
+     
+
+        </div>
+    </div>
+)
 }
