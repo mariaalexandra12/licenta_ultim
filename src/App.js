d56b6683e9e91c4da175789b624769326eb6eeb1
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-11 14:41:49 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Navig from "./components/Navig";
 
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import Rapoarte from "./pages/Rapoarte/Rapoarte";
+import Analiza from "./pages/Analiza/Analiza";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AdaugaFacturi from "./pages/AdaugaFacturi/AdaugaFacturi";
+import Facturi from "./pages/Facturi/Facturi"
+import Auth from "./pages/Authentification/Auth";
+
+
 
 function App() {
   return (
-    <div>
-     <Navig/>
-    </div>
+    <React.StrictMode>
+    <BrowserRouter >
+          <Routes>
+            <Route exact path="/" element={<Auth/>}></Route>
+            <Route  path="navig" element={<Navig/>}></Route>
+              <Route path="navig/analiza" element={<Analiza/>}></Route>
+              <Route path="navig/dashboard" element={<Dashboard/>}></Route>
+              <Route path="navig/adaugaFacturi" element={<AdaugaFacturi/>}></Route>
+              <Route path="navig/facturi" element={<Facturi/>}></Route>
+              
+            </Routes>
+
+    </BrowserRouter>
+  </React.StrictMode>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,33 +3,14 @@ import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter,Route,Routes} from "react-router-dom";
-import Rapoarte from "./pages/Rapoarte/Rapoarte";
-import Analiza from "./pages/Analiza/Analiza";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import AdaugaFacturi from "./pages/AdaugaFacturi/AdaugaFacturi";
-import Facturi from "./pages/Facturi/Facturi"
-import Auth from "./pages/Authentification/Auth";
-import Navig from './components/Navig';
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter >
-          <Routes>
-            <Route exact path="/" element={<Auth/>}></Route>
-            <Route  path="navig" element={<Navig/>}></Route>
-              <Route path="navig/analiza" element={<Analiza/>}></Route>
-              <Route path="navig/dashboard" element={<Dashboard/>}></Route>
-              <Route path="navig/adaugaFacturi" element={<AdaugaFacturi/>}></Route>
-              <Route path="navig/facturi" element={<Facturi/>}></Route>
-              
-            </Routes>
-
-    </BrowserRouter>
-  </React.StrictMode>
+  <div>
+    <App/>
+  </div>
+  
 );
 
 
