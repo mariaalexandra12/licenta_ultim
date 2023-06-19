08ed397ad525eecf463255f5457014010a5eef94
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-19 16:39:13 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -46,7 +46,7 @@ export default function AdauagaFacturi(){
         setDueDate(' ');
       }
 
-    const patternValue=/[Total de plata]:\s*([0-9]+)\s*lei/gmi;
+    const patternValue=/Total de plata\s*:\s*(\d+(?:\.\d+)?)\b/gmi;
     const matchValue=out.data.text.match(patternValue);
     if(matchValue && matchValue[1]){
       setTotalValue(matchValue[1]);
