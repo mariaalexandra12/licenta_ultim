53b7c0f8964e812275c399afef0d31a27c7fc177
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-20 19:21:49 +0300

    commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express=require('express');
+const app = express();
+
+app.get("/",function(res,req){
+    res.send("buna");
+})
+
+
+app.listen(3002,function(){
+    console.log('running in 3002');
+})
