053c910d0fd88f96dc42ea431720fd1f1a6ff386
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-20 17:29:12 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import "./uploader.css";
+import { Button } from '@mui/material/Button'
 
 
 export default function AdauagaFacturi(){
-  
-  const express=require("express");
-  const app = express();
 
 
   return(    
     <div className="adaugaFact">
         <span>Selecteaza un document</span>
         <input className='selectfile' type="file"></input>
+        <Button variant="outlined" >Extrage date</Button>
         <span id="result">Result text</span>
         <textarea className="text" ></textarea>
   </div>
