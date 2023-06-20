d553a0df49e2382bdffb7598f5337feeee362667
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-20 21:22:34 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState , useEffect ,useRef} from "react";
 import "./uploader.css";
-import { Button } from '@mui/material'
-
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 export default function AdauagaFacturi(){
 
   
- 
+  const [progress,setProgress]=useState(0);
+  const [progressLabel , setProgressLabel]=useState('idle');
+  const [imageData, setImageData]=useState();
+  const [ocrResult,setOcrResult]=useState('');
+
 
   return(    
     <div className="adaugaFact">
@@ -15,7 +20,11 @@ export default function AdauagaFacturi(){
         <Button variant="contained" color="success" style={{
           marginTop:'25px'
         }}>Extrage date</Button>
-        <span id="result">Result text</span>
+        <LinearProgress variant="determinate" value={progress} style={{
+          width:'250px',
+          marginTop:'20px',
+          }}></LinearProgress>
+        <span id="result" style={{marginTop:'10px'}}>Result text</span>
         <textarea className="text" ></textarea>
   </div>
 )
--- src/pages/Authentification/logIn.js
+++ src/pages/Authentification/logIn.js
@@ -1,150 +1,112 @@
-import React, { useState } from "react";
-import { Chip} from "@mui/material";
-import LockOpenIcon from '@mui/icons-material/LockOpen';
-import TextField from '@mui/material/TextField';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
-import Stack from '@mui/material/Stack';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import FilledInput from '@mui/material/FilledInput';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import { useNavigate} from "react-router-dom";
-import Alert from '@mui/material/Alert';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-
-
-function LogIn(){
-
-    const [showPassword, setShowPassword] = React.useState(false);
-
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-
-    const [email,setEmail]=useState();
-    const [password,setPassword]=useState();
-   
-    const [emailError,setEmailError]=useState();
-    const [passwordError,setPasswordError]=useState();
-
-    const handleEmail=()=>{
-        if(!email)
-        {
-            setEmailError(true);
-            return;
-        }
-
-    if(!email.endsWith("@gmail.com")){
-        setEmailError(true);
-        alert("Please enter a valid email address!");
-        return;
-    }
-
-    setEmailError(false);
-    }
-
-  
-
-    const handlePass=()=>{
-    if(!password || password.length < 8 || password.length > 12){
-        setPasswordError(true);
-        alert("Please enter a valid password");
-        return;
-    }
-
-    setPasswordError(false);
-    }
-
-    
-    const [valid, setValid]=useState();
-
-    const handleLogIn=()=>{
-
-      if(!password){
-         setValid('Please enter a valid password.The password shoul have between 8 and 15 characters!');
-      }
-      if(!email){
-        setValid('Please enter a valid email address!')
-      }
-
-     };
-
-     const nav=useNavigate();
-      
-     
-    return (
-        <div>
-          <Chip label="Log In" variant="outlined"
-           icon={<LockOpenIcon/>} color="secondary"
-           style={{marginLeft: "150px", marginTop: "20px"}}
-           />
-          <br/>
-          <p>
-          <TextField
-          id="filled-error-helper-text"
-          label="Email Adress"
-          helperText="Please enter your Email Adress."
-          variant="filled"
-          color="secondary"
-          style={{marginTop:30,width:"80%"}}
-          value={email}
-          onChange={(event) =>setEmail(event.target.value)}
-          error={emailError}
-          onBlur={handleEmail}
-        />
-        </p>
-          <FormControl sx={{marginBottom:5,width:"80%"}} variant="filled" 
-          >
-          <InputLabel htmlFor="filled-adornment-password"
-          color="secondary" form>Password</InputLabel>
-          <FilledInput
-            id="filled-adornment-password" 
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            error={passwordError}
-            onBlur={handlePass}
-          onChange={(event) =>setPassword(event.target.value)}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-              />
-              </FormControl>
-
-          <br/>
-
-
-        <Stack>
-
-        <Button color="secondary" variant="outlined"
-        startIcon={<LoginOutlinedIcon/>} style={{width:"80%"}}
-        onClick={()=>nav("navig")}>Log In</Button> 
-        </Stack> 
-
-        {valid && (
-           <Stack sx={{ width: '100%' }} spacing={2}>
-             <Alert severity="error" size="small">{setValid}</Alert>
-           </Stack>
-        )}
-
-          
-    </div>
-    )
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
 
-export default LogIn;
+const defaultTheme = createTheme();
+
+export default function LogIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              color="secondary"
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
