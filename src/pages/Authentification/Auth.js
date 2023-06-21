0344e90a82477b255b036815a8263d4d203068f8
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-21 16:15:39 +0300

    commit

--- src/pages/Authentification/Auth.js
+++ src/pages/Authentification/Auth.js
@@ -13,29 +13,28 @@ function Auth(){
     setChecked(event.target.checked);
     };
 
-
-    
     return (
-        <div className="auth" >   
-          <Paper elevation={24} style={{
-            marginLeft:"400px",
-            marginTop:"50px",
-            width:"50%",
-            height:"50%",
-            color:"primary",
-            padding:"10px",
-            background: "rgba( 189, 16, 224,0.10)",
-            boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-            backdropFilter: "blur( 0px )",
-            WebkitBackdropFilter: "blur( 0px )",
-             borderRadius:" 10px",
-            border: "1px solid rgba( 255, 255, 255, 0.18 )",}} square="true">
-           <Switch defaultChecked color="secondary" onChange={handleChange}
-            checked={checked} inputProps={{'aria-label':"controlled"}}/>
-            <br/>
-            {checked ?(<LogIn/>):(<SignUp/>)}
-            </Paper>
-         </div>
+      <div className="auth" >   
+         <div className="container">
+            <Paper elevation={24} style={{
+               marginLeft:"350px",
+               marginTop:"55px",
+               width:"50%",
+               color:"primary",
+               padding:"10px",
+               background: "rgba( 189, 16, 224,0.10)",
+                boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+                backdropFilter: "blur( 0px )",
+                WebkitBackdropFilter: "blur( 0px )",
+                borderRadius:" 10px",
+                border: "1px solid rgba( 255, 255, 255, 0.18 )",}} square="true">
+             <Switch defaultChecked color="secondary" onChange={handleChange}
+             checked={checked} inputProps={{'aria-label':"controlled"}}/>
+             <br/>
+             {checked ?(<LogIn/>):(<SignUp/>)}
+             </Paper>
+        </div>
+     </div>
     
     );
 }
