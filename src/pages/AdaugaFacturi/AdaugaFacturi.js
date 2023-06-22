2bf2e05e70e31fdefe539fdd2cfe60af61389c7c
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-22 17:15:11 +0300

    commit

--- src/pages/AdaugaFacturi/AdaugaFacturi.js
+++ src/pages/AdaugaFacturi/AdaugaFacturi.js
@@ -1,13 +1,22 @@
-import React,{ useState } from "react";
+import React,{ useState , useCallback} from "react";
 import "./uploader.css";
 import { Box, Button } from '@mui/material';
 import LinearProgress from '@mui/material/LinearProgress';
 import Navig from "../../components/Navig";
-
+import {useDropzone} from 'react-dropzone'
 
 function AdaugaFacturi(){
-  const [file, setFile]=useState();
+  const [selectedFiles, setSelectedFiles]=useState();
+  const onDrop = useCallback(acceptedFiles => {
+    setSelectedFiles(acceptedFiles.map(file =>
+      Object.assign(file,{
+        preview:URL.createObjectURL(file)
+      })
+      
+      ));
+  }, [])
 
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
 
   return(    
     <>
@@ -16,10 +25,17 @@ function AdaugaFacturi(){
     <Box sx={{marginTop: '50px',}}>
       <Box sx={{marginLeft:'15px'}}>
       <h2>Incarca o factura</h2>
-      <form>
-       <input type="file" name="file" ></input>
-       <Button varinat="outlined" color="secondary">Incarca</Button>
-      </form>
+      <div>
+      <input {...getInputProps()} />
+          <p>Insereaza o factura aici</p> :
+
+
+
+
+
+      </div>
+    
+    
       </Box>
     </Box>
 
