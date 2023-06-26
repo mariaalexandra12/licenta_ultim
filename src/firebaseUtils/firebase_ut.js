eb61c2cb4ea5c4d140deae860809737f5d188ca0
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-26 14:53:05 +0300

    commit

--- src/firebaseUtils/firebase_ut.js
+++ src/firebaseUtils/firebase_ut.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db=getFirestore();
+const db=getFirestore(app);
 const storage=getStorage();
 const auth=getAuth(app);
 export {db,app,storage,auth};
