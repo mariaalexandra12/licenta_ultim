ebcada0cff47c0223247021c2a23351dcf18cd68
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-27 19:21:32 +0300

    commit

--- src/pages/Authentification/forgetParola.js
+++ src/pages/Authentification/forgetParola.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
+import { TextField, Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import ReceiptIcon from '@mui/icons-material/Receipt';
 
 
 export default function Pass(){
@@ -9,8 +12,8 @@ export default function Pass(){
      <Paper elevation={24} style={{
                marginLeft:"350px",
                marginTop:"55px",
-               width:"550px",
-               height:'500px',
+               width:"500px",
+               height:'400px',
                color:"primary",
                padding:"10px",
                background: "rgba( 189, 16, 224,0.10)",
@@ -19,7 +22,26 @@ export default function Pass(){
                 WebkitBackdropFilter: "blur( 0px )",
                 borderRadius:" 10px",
                 border: "1px solid rgba( 255, 255, 255, 0.18 )",}} square="true">
-                    </Paper>
+                <ReceiptIcon sx={{fontSize:70,
+                 marginLeft:'200px',
+                 marginTop:'40px'
+                }} color='secondary'/>
+                <Typography style={{marginTop:'40px',
+                 textAlign:'center'}}>Introdu aici adresa de email
+                    si iti vom trimite un mail 
+                    pentru resetarea parolei.
+                </Typography>
+
+                <TextField type='text' style={{
+                    marginTop:'50px',
+                }} fullWidth
+                placeholder='Adresa de email'
+                ></TextField>
+
+                <Button color="secondary" variant="contained" fullWidth
+                 style={{marginTop:'10px'}}
+                >Trimite mail</Button>
+    </Paper>
      
      </div>
 
