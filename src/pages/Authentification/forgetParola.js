aa469253d215c47d41091a7fd4df3d1f2829a433
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-27 18:18:33 +0300

    commit

--- src/pages/Authentification/forgetParola.js
+++ src/pages/Authentification/forgetParola.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Paper from '@mui/material/Paper';
 
 
 export default function Pass(){
     return (
      <div>
      
-     <h1>Forget cu trimite mail cu firebase cu auth passs</h1>
+     <Paper elevation={24} style={{
+               marginLeft:"350px",
+               marginTop:"55px",
+               width:"550px",
+               height:'500px',
+               color:"primary",
+               padding:"10px",
+               background: "rgba( 189, 16, 224,0.10)",
+                boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+                backdropFilter: "blur( 0px )",
+                WebkitBackdropFilter: "blur( 0px )",
+                borderRadius:" 10px",
+                border: "1px solid rgba( 255, 255, 255, 0.18 )",}} square="true">
+                    </Paper>
      
      </div>
 
