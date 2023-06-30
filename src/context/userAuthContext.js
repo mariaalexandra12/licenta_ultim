e0d12c2d8fe9acf9d75479798cab4f13612ffda2
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-06-30 21:56:06 +0300

    commit

--- src/context/userAuthContext.js
+++ src/context/userAuthContext.js
@@ -1,12 +1,12 @@
 import { createContext, useContext,useEffect,useState} from "react";
 import {auth} from '../firebaseUtils/firebase_ut';
 import {createUserWithEmailAndPassword,
-    onAuthStateChanged
+    onAuthStateChanged 
 } from 'firebase/auth'
 
-const AuthContext = createContext({
-    currentUser:null,
-});
+import * as firebase from 'firebase/auth';
+
+const AuthContext = createContext();
 
 export function AuthContextProvider({children}) {
     
@@ -15,15 +15,22 @@ export function AuthContextProvider({children}) {
     const [password,setPassword]=useState('') 
 
     useEffect(()=>{
+    
       const unsubscribe=onAuthStateChanged(auth,user=>{
         if(user){
+            const userKey=user.uid;
+            localStorage.setItem(userKey,user.email);
             setCurrentUser(user.email)
+        }
+        else{
+            setCurrentUser('');
+            const key=user.uid;
+            localStorage.removeItem(key);
         }
       })
       return ()=>{
         unsubscribe();}
     },[])
-
 
     const value={
         currentUser,
@@ -38,4 +45,18 @@ export function AuthContextProvider({children}) {
 
 export function useUserAuth(){
     return useContext(AuthContext);
+}
+
+export function getFromLocalStorage(mail){
+    for (let i=0;i<localStorage.length;i++){
+        const key=localStorage.key(i);
+        if(key.includes('@gmail.com')){
+            const getEmail=localStorage.getItem(key);
+            if(getEmail === mail)
+            {
+                 return getEmail;
+            }
+        }
+    }
+     return null;
 }
