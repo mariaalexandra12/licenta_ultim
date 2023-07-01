6a7ee3641a22fd3121f1e77fbcfdb0cafddcd593
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-07-01 20:55:13 +0300

    commit

--- src/context/userAuthContext.js
+++ src/context/userAuthContext.js
@@ -10,53 +10,30 @@ import { collection, query, where, getDocs,onSnapshot, QuerySnapshot} from "fire
 
 const AuthContext = createContext({
     currentUser:null,
-    dateLogare:null,
 });
 
 export function AuthContextProvider({children}) {
     
     const [currentUser,setCurrentUser]=useState(null) 
-    const [dateLogare,setDateLogare]=useState('') 
 
     useEffect(()=>{
       const unsubscribe=onAuthStateChanged(auth,user=>{
         if(user){
             setCurrentUser(user.email);
-            // localStorage.setItem(user.uid,user.email);
-                const q2 = query(collection(db, "utilizator"),where("emailUtilizator","==",user.email));
-                onSnapshot(q2,(snapshot)=>{
-               let userData=[];
-               snapshot.docs.forEach((doc)=>{
-                // if(doc.data()){
-                    userData.push({...doc.data,id:doc.id});
-            //    } 
-              }
-             )
-              setDateLogare(userData);
-            });
-
-                //  const q=query(collection(db,"firma"),where("emailFirma","==",user.email));
-                //  let dateFirma=[];
-                //  onSnapshot(q,(snapshot)=>{
-                //     snapshot.docs.forEach((doc)=>{
-                //     if(doc.data()){
-                //         dateFirma.push({...doc.data(),id:doc.id});
-                //    }
-                //  })
-                // setDateLogare(JSON.stringify(dateFirma));
-                // })
-
                 }
       return ()=>{
         unsubscribe();
-        setDateLogare([]);}
+        }
     })
 },[])
 
+const value={
+    currentUser
+}
 
     return (
         // value={currentUser}
-        <AuthContext.Provider value={{currentUser,dateLogare}}>
+        <AuthContext.Provider value={value} >
             {children}
         </AuthContext.Provider> 
     )
