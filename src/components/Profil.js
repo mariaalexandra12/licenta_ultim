1cba6f970a6733d1af6af3159adb65f4820aecbb
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-07-01 15:59:46 +0300

    commit

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -4,7 +4,6 @@ import Navig from "./Navig";
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
-import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import PersonPinIcon from '@mui/icons-material/PersonPin';
@@ -18,158 +17,21 @@ import { collection, query, where, getDocs,onSnapshot, QuerySnapshot} from "fire
 
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+
+
 
 const Profil=()=>{
 
-    const [value, setValue] = React.useState(0);
+    
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-
-   const { currentUser }= useUserAuth()
-
-   const [pass,setPass]=useState('')
-   const [nume,setNume]=useState('')
-   const[prenume,setPrenume]=useState('')
-  
-  //  DATE FIRMA
-
-  const [cif,setCIF]=useState('');
-  const [denumire,setDenumire]=useState('');
-  const[judet,setJudet]=useState('');
-  const[local,setLocal]=useState('');
-  const [parolaFirma,setParolaFirma]=useState('');
-  const [platitor,setPlatitor]=useState('');
-
-  const existaPers=useRef(false);
-
-   const [existaContPers,setExistaContPers]=useState(false);
-
-   
-   useEffect(() => {
-
-    if (currentUser) {
-      let idU;
-
-
-      const q = query(collection(db, "utilizator"), 
-         where("emailUtilizator","==",currentUser));
-      onSnapshot(q,(snapshot)=>{
-        let userData=[];
-        snapshot.docs.forEach((doc)=>{
-          if(doc.data()){
-            idU=doc.id;
-           existaPers.current=true;
-            userData.push({...doc.data(), id:doc.id})
-            localStorage.setItem(doc.id, JSON.stringify(doc.data()));
-            setExistaContPers(true);
-          } 
-     })
-
-     if(existaPers.current.valueOf() === true){
-        const dateUser=localStorage.getItem(idU);  
-          const numeReg=/("nume":")([a-zA-Z]+)/gmi;
-          const prenumeReg=/("prenume":")([a-zA-Z]+)/gmi;
-          const parolaReg=/"parolaUtilizator":"([^"]+)"/gmi;
-  
-          const numeMatch=dateUser.match(numeReg);
-          const prenumeMatch=dateUser.match(prenumeReg);
-          const parolaMatch=dateUser.match(parolaReg);
-  
-          const numeU=numeMatch && numeMatch[0] ? (numeMatch[0].replace('"nume":"','')) : ' ';
-          const prenumeU=prenumeMatch && prenumeMatch[0] ? (prenumeMatch[0].replace('"prenume":"','')) : ' ';
-          const parola=parolaMatch && parolaMatch[0] ? (parolaMatch[0].replace('"parolaUtilizator":"','')) : ' ';
-  
-          setNume(numeU);
-          setPrenume(prenumeU);
-          setPass(parola);
-          // setDenumire('');
-          // setCIF('');
-          // setJudet('');
-          // setLocal('');
-          // setParolaFirma('');
-          // setPlatitor('');
-          console.log('am intrat pe eixstaPers TRUE ');
-        
-     }
-     }
-     ) 
-
-    // existaPers.current=false;     
-
-     const q2 = query(collection(db, "firma"));
-     let idF;
-     where("emailFirma","==",currentUser);
-     onSnapshot(q2,(snapshot)=>{
-    let firmaData=[];
-    snapshot.docs.forEach((doc)=>{
-      if(doc.data()){
-        firmaData.push({...doc.data(), id:doc.id});
-        idF=doc.id;
-        localStorage.setItem(doc.id, JSON.stringify(doc.data()));
-        // localStorage.setItem()
-      }
-        })
-   
-    if(existaPers.current.valueOf() === false){
-     const dateFirma=localStorage.getItem(idF);     
-           const denumireFirmaReg=/([a-zA-Z])+\s+.*SRL/gmi;
-            const denumireFirmaMatch=dateFirma.match(denumireFirmaReg);
-            const denFirma=denumireFirmaMatch && denumireFirmaMatch[0] ? denumireFirmaMatch[0]:'';
-            // setDenumire(denFirma);
-            console.log(denFirma)
-
-              // console.log('am intrat pe existPers FALSE');
-            const cifReg=/("CIF":")\s*[0-9]*/gmi;
-            const cifMatch=dateFirma.match(cifReg);
-            const cifFirma=cifMatch && cifMatch[0] ? cifMatch[0] : '';
-            setCIF(cifFirma.replace('"CIF":"',''));
-            //console.log(cifFirma.replace('"CIF":"',''))
-
-
-            const judetReg=/("judet":")\s*[a-zA-Z]*/gmi;
-            const judetMatch=dateFirma.match(judetReg);
-            const judetFirma=judetMatch && judetMatch[0] ? judetMatch[0] : '';
-            setJudet(judetFirma.replace('"judet":"',''));
-            // console.log(judetFirma.replace('"judet":"',''))
-     
-
-            const localitateRegex=/("localitate":")\s*[a-zA-Z]*/gmi;
-            const localitateMatch=dateFirma.match(localitateRegex);
-            const localFirma=localitateMatch && localitateMatch[0] ? localitateMatch[0] : '';
-            setLocal(localFirma.replace('"localitate":"',''));
-            // console.log(localFirma.replace('"localitate":"',''))
-
-
-            const parolaFirmaRegex=/("parolaFirma":")\s*[a-zA-Z]*/gmi;
-            const parolaFirmaMatch=dateFirma.match(parolaFirmaRegex);
-            const parolFir=parolaFirmaMatch && parolaFirmaMatch[0] ? parolaFirmaMatch[0] : '';
-           setParolaFirma(parolFir.replace('"parolaFirma":"',''));
-            // console.log(parolFir.replace('"parolaFirma":"',''))
-
-
-            const platTVARegex=/("platitorTva":")\s*[a-zA-Z]*/gmi;
-            const platTVAMatch=dateFirma.match(platTVARegex);
-            const platTVA=platTVAMatch && platTVAMatch[0] ? platTVAMatch[0] : '';
-            if(platTVA.replace('"platitorTVA":"','') === "on"){
-              
-              setPlatitor("DA");
-            }
-            else{
-              setPlatitor("NU");
-            }
-          }
-        }
-     ) 
-  }
-  }, [currentUser]);
 
 
     return (
@@ -203,7 +65,7 @@ const Profil=()=>{
               
               </Grid>
 
-              <Grid item >
+              {/* <Grid item >
               <Item style={{
                 marginTop:'50px',
                 width:'1000px',
@@ -215,265 +77,10 @@ const Profil=()=>{
                 borderRadius:' 10px',
                 border: '1px solid rgba( 255, 255, 255, 0.18 )',
               }}>
-       
-       { (existaContPers.valueOf() === true) ? 
-        <>
-       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} textColor="secondary" >
-          <Tab label="Cont Personal" />
-          <Tab label="Cont Firma"  />
-        </Tabs>
-      </Box>
-
-      { value===0 &&(
-        <>
-          {/* CONT PERSONAL  */}
-          {/**/}
-           <Box  sx={{ display: 'flex', alignItems: 'flex-end',}}  >
-              <Typography >Adresa de Email</Typography>
-              <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.2,marginLeft:'30px' }} />
-              <TextField id="adresaEmail" 
-              type='text'
-              variant="standard" 
-              defaultValue={currentUser}
-              style={{
-                width:'300px',
-                marginTop: '30px'
-              }}
-              />
-           </Box>
-
-             
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Parola Cont</Typography>
-              <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.2,marginLeft:'60px' }} />
-              <TextField id="input-with-sx" 
-              type="text"
-              variant="standard" 
-              style={{
-                width:'300px',
-              }}
-              defaultValue={pass}
-              />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Nume Utilizator</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1,  my: 0.2,marginLeft:'33px' }} />
-              <TextField id="input-with-sx" variant="standard" 
-               type="text"
-               defaultValue={nume}
-               style={{
-                width:'300px',
-              }}/>
-           </Box>
-
-           
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Prenume Utilizator</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.2,marginLeft:'10px' }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              defaultValue={prenume}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-        </>
-      )}
-
-       {value===1 &&(
-        <>
-          {/* CONT FIRMA */}
-           <Box sx={{ display: 'flex', alignItems: 'flex-end',}}>
-           <Typography >Adresa de email Firma</Typography>
-              <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="adresaEmail" 
-              type='text'
-              variant="standard" 
-              // defaultValue={currentUser}
-              style={{
-                width:'300px',
-                marginTop: '30px',
-                
-              }}
-              />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Parola Cont Firma</Typography>
-              <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx" 
-              type="text"
-              variant="standard" 
-              style={{
-                width:'300px',
-              }}
-              // defaultValue={parolaFirma}
-              />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Denumire Firma</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx" variant="standard" 
-               type="text"
-              //  defaultValue={denumire}
-               style={{
-                width:'300px',
-              }}/>
-           </Box>
-
-           
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Cod Inregistrare Fiscala</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              // defaultValue={cif}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography>Judet</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              // defaultValue={judet}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Localitate</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              // defaultValue={local}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Platitor TVA</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              // defaultValue={platitor}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-        </>
-      )}        
-      </>  
-      
-      :
-
-      <>
-       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} textColor="secondary" >
-          <Tab label="Cont Firma"  />
-        </Tabs>
-      </Box>
-
-       {value===0 &&(
-        <>
-          {/* CONT FIRMA */}
-           <Box sx={{ display: 'flex', alignItems: 'flex-end',}}>
-           <Typography >Adresa de email Firma</Typography>
-              <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="adresaEmail" 
-              type='text'
-              variant="standard" 
-              defaultValue={currentUser}
-              style={{
-                width:'300px',
-                marginTop: '30px',
-                
-              }}
-              />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Parola Cont Firma</Typography>
-              <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx" 
-              type="text"
-              variant="standard" 
-              style={{
-                width:'300px',
-              }}
-              defaultValue={parolaFirma}
-              />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Denumire Firma</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx" variant="standard" 
-               type="text"
-               defaultValue={denumire}
-               style={{
-                width:'300px',
-              }}/>
-           </Box>
-
-           
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Cod Inregistrare Fiscala</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              defaultValue={cif}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography>Judet</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              defaultValue={judet}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Localitate</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              defaultValue={local}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-
-           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-              <Typography >Platitor TVA</Typography>
-              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx"  variant="standard"
-              type="text"
-              defaultValue={platitor}
-               style={{
-                width:'300px',
-              }} />
-           </Box>
-        </>
-      )}        
-      </>
-
-      }
               </Item>
-              </Grid>
+              </Grid> */}
+
+
              
            </Grid>
            
---
+++ src/components/contPers.js
@@ -0,0 +1,260 @@
+import React,{ useState,useEffect,useRef } from "react";
+import { Box, Button, Divider, Icon, IconButton, Typography } from '@mui/material';
+import Navig from "./Navig";
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import EmailIcon from '@mui/icons-material/Email';
+import TextField from '@mui/material/TextField';
+import { useUserAuth} from "../context/userAuthContext";
+import HttpsIcon from '@mui/icons-material/Https';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+import { db } from "../firebaseUtils/firebase_ut";
+import { collection, query, where, getDocs,onSnapshot, QuerySnapshot} from "firebase/firestore";
+import Profil from "./Profil";
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+
+
+export default function ContPers(){
+   
+    const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+ const { currentUser }= useUserAuth()
+ const [ pass , setPass]=useState('');
+ const [name, setName]=useState('');
+ const [prename, setPrename]=useState('');
+
+ useEffect(() => {
+
+    const q2 = query(collection(db, "utilizator"));
+    let idF;
+    where("emailUtilizator","==",currentUser);
+    onSnapshot(q2,(snapshot)=>{
+   let userData=[];
+   snapshot.docs.forEach((doc)=>{
+     if(doc.data()){
+       userData.push({...doc.data(), id:doc.id});
+       idF=doc.id;
+       localStorage.setItem(doc.id, JSON.stringify(doc.data()));
+       // localStorage.setItem()
+     }
+       })
+  
+   
+    const dateUser=localStorage.getItem(idF);     
+    const prenameRegex=/(prenume:)\s*(")[a-zA-Z]*/gmi;
+    const prenameMatch=dateUser.match(prenameRegex);
+    const prename=prenameMatch && prenameMatch[0] ? prenameMatch[0] : '';
+    console.log(prename);
+    
+    const nameRegex=/(prenume:)\s*(")[a-zA-Z]*/gmi;
+    const nameMatch=dateUser.match(nameRegex);
+    const name=nameMatch && nameMatch[0] ? nameMatch[0] : '';
+    console.log(name);
+
+    const parolaRegex=/(prenume:)\s*(")[a-zA-Z]*/gmi;
+    const parolaMatch=dateUser.match(parolaRegex);
+    const parola=parolaMatch && parolaMatch[0] ? parolaMatch[0] : '';
+    console.log(parola);
+
+         })
+
+    },[currentUser]) 
+
+
+    return (
+        <>
+        <Box sx={{display: 'flex'}}>
+         <Box sx={{  marginTop:'80px',
+      marginLeft:'20px',}}>
+        <Profil/>
+
+              <Grid item >
+              <Item style={{
+                marginTop:'50px',
+                width:'1000px',
+                height:'400px',
+                background: 'rgba( 186, 152, 224, 0.7 )',
+                boxShadow:'0 8px 32px 0 rgba( 31, 38, 135, 0.37 )', 
+                backdropFilter:' blur( 11px )',
+                WebkitBackdropFilter:' blur( 11px )',
+                borderRadius:' 10px',
+                border: '1px solid rgba( 255, 255, 255, 0.18 )',
+              }}>
+        
+        <>
+       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} textColor="secondary" >
+          <Tab label="Cont Personal" />
+          <Tab label="Cont Firma"  />
+        </Tabs>
+      </Box>
+
+      { value===0 &&(
+        <>
+          {/* CONT PERSONAL  */}
+          {/**/}
+           <Box  sx={{ display: 'flex', alignItems: 'flex-end',}}  >
+              <Typography >Adresa de Email</Typography>
+              <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.2,marginLeft:'30px' }} />
+              <TextField id="adresaEmail" 
+              type='text'
+              variant="standard" 
+              defaultValue={currentUser}
+              style={{
+                width:'300px',
+                marginTop: '30px'
+              }}
+              />
+           </Box>
+
+             
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Parola Cont</Typography>
+              <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.2,marginLeft:'60px' }} />
+              <TextField id="input-with-sx" 
+              type="text"
+              variant="standard" 
+              style={{
+                width:'300px',
+              }}
+              defaultValue={pass}
+              />
+           </Box>
+
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Nume Utilizator</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1,  my: 0.2,marginLeft:'33px' }} />
+              <TextField id="input-with-sx" variant="standard" 
+               type="text"
+               defaultValue={name}
+               style={{
+                width:'300px',
+              }}/>
+           </Box>
+
+           
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Prenume Utilizator</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.2,marginLeft:'10px' }} />
+              <TextField id="input-with-sx"  variant="standard"
+              type="text"
+              defaultValue={prename}
+               style={{
+                width:'300px',
+              }} />
+           </Box>
+        </>
+      )}
+
+       {value===1 &&(
+        <>
+          {/* CONT FIRMA */}
+           <Box sx={{ display: 'flex', alignItems: 'flex-end',}}>
+           <Typography >Adresa de email Firma</Typography>
+              <EmailIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="adresaEmail" 
+              type='text'
+              variant="standard" 
+              style={{
+                width:'300px',
+                marginTop: '30px',
+                
+              }}
+              />
+           </Box>
+
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Parola Cont Firma</Typography>
+              <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="input-with-sx" 
+              type="text"
+              variant="standard" 
+              style={{
+                width:'300px',
+              }}
+              />
+           </Box>
+
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Denumire Firma</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="input-with-sx" variant="standard" 
+               type="text"
+               style={{
+                width:'300px',
+              }}/>
+           </Box>
+
+           
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Cod Inregistrare Fiscala</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="input-with-sx"  variant="standard"
+              type="text"
+               style={{
+                width:'300px',
+              }} />
+           </Box>
+
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography>Judet</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="input-with-sx"  variant="standard"
+              type="text"
+               style={{
+                width:'300px',
+              }} />
+           </Box>
+
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Localitate</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="input-with-sx"  variant="standard"
+              type="text"
+               style={{
+                width:'300px',
+              }} />
+           </Box>
+
+           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography >Platitor TVA</Typography>
+              <AccessibilityNewIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              <TextField id="input-with-sx"  variant="standard"
+              type="text"
+               style={{
+                width:'300px',
+              }} />
+           </Box>
+        </>
+      )}        
+
+      </>  
+
+              </Item>
+              </Grid>
+             
+           </Box>
+           
+        </Box>
+    
+        </>
+    )
+
+}
