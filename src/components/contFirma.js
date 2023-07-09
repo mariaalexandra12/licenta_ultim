274c5dafe36830298b46c6b714578c8d79d0e400
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-07-09 14:48:14 +0300

    commit

--- src/components/contFirma.js
+++ src/components/contFirma.js
@@ -86,12 +86,10 @@ const ContFirma=()=>{
                 width:'1000px',
                 display:'flex',
                 flexDirection:'row',
-                background: 'rgba( 186, 152, 224, 0.7 )',
-                boxShadow:'0 8px 32px 0 rgba( 31, 38, 135, 0.37 )', 
-                backdropFilter:' blur( 11px )',
-                WebkitBackdropFilter:' blur( 11px )',
-                borderRadius:' 10px',
-                border: '1px solid rgba( 255, 255, 255, 0.18 )',
+                background: 'rgba( 189, 16, 224, 0.25 )',
+                boxShadow:'0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                backdropFilter: 'blur( 4px )',
+                WebkitBackdropFilter:' blur( 4px )',
             }}>
                 <Typography style={{fontSize:'35px'}}>Profil</Typography>
                 <IconButton style={{marginLeft:'450px',}}><PersonPinIcon/></IconButton>
