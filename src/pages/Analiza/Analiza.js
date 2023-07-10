4d39b06d6a33d195f8270591c0a290d46d62f79b
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-07-10 23:39:04 +0300

    commit

--- src/pages/Analiza/Analiza.js
+++ src/pages/Analiza/Analiza.js
@@ -4,6 +4,8 @@ import { Box, Button } from '@mui/material';
 import { useUserAuth } from '../../context/userAuthContext';
 import { collection, query, where, onSnapshot, doc, deleteDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebaseUtils/firebase_ut';
+import Paper from '@mui/material/Paper';
+
 
 
 const Analiza=()=>{
@@ -11,6 +13,12 @@ const Analiza=()=>{
      const { currentUser }=useUserAuth();
      const [dateFactura, setDateFactura] = useState([]);
      const [idDoc, setIdDoc] = useState('');
+
+     const [dataScadenta,setDataScadenta] = useState([]);
+     const [tipFactura,setTipFactura]=useState([]);
+     const [valoareTotala,setValoareTotala] = useState([]);
+     const [numeFurnizor,setNumeFurnizor]=useState([]);
+
 
      useEffect(() => {
       const q2 = query(collection(db, 'factura'), where('emailUtilizator', '==', currentUser));
@@ -25,16 +33,48 @@ const Analiza=()=>{
       return () => {
         unsub();
       };
-      console.log(dateFactura);
+      
     }, [currentUser]);
 
+    useEffect(()=>{
+       dateFactura.forEach((fact)=>{
+        dataScadenta.push(fact['dataScadenta']);
+        numeFurnizor.push(fact['numeFurnizor']);
+        tipFactura.push(fact['tipFactura']);
+        valoareTotala.push(fact['valoareTotala']);
+       })
+    },[dateFactura])
+
+    useEffect(()=>{
+       console.log(dataScadenta);
+       console.log(numeFurnizor);
+       console.log(tipFactura);
+       console.log(valoareTotala);
+    },[dataScadenta,numeFurnizor,tipFactura,valoareTotala])
     
     return (
         <>
         <Box sx={{display: 'flex'}}>
         <Navig/>
-        <Box sx={{marginTop: '80px'}}>
-       
+        <Box sx={{marginTop: '30px'}}>
+          <Paper elevation={24} style={{
+            width:"400px",
+            height:'400px',
+            marginLeft:'20px',
+            marginTop:'10px',
+            background: 'rgba( 228, 189, 189, 0.25 )',
+            boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+            backdropFilter: 'blur( 4px )',
+            WebkitBackdropFilter: 'blur( 4px )',
+            borderRadius: '10px',
+            border: '1px solid rgba( 255, 255, 255, 0.18 )',}}>
+            </Paper>
+
+
+
+
+
+
         </Box>
     
         </Box>
